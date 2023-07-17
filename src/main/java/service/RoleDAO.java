d3e676237b5ebda86a8391ac90f7b961f04df55f
rudskaya irina <irarudskaya5@gmail.com>
2023-07-18 01:36:06 +0300

    Role

--- src/main/java/service/RoleDAO.java
+++ src/main/java/service/RoleDAO.java
@@ -1,4 +1,14 @@
 package service;
 
+import model.Role;
+
+import java.util.List;
+
 public interface RoleDAO {
+
+    Role addRole(Role role);
+    Role getById(Long id);
+    List<Role> getAllRoles();
+    void deleteRole(Role role);
+
 }
--- src/main/java/service/RoleDAOImpl.java
+++ src/main/java/service/RoleDAOImpl.java
@@ -1,5 +1,46 @@
 package service;
 
+import model.Role;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class RoleDAOImpl implements RoleDAO {
+
+    @Override
+    public Role addRole(Role role) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(role);
+            transaction.commit();
+            session.close();
+        }
+        return role;
+    }
+
+    @Override
+    public Role getById(Long id) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.get(Role.class, id);
+        }
+    }
+
+    @Override
+    public List<Role> getAllRoles() {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Role").list();
+        }
+    }
+
+    @Override
+    public void deleteRole(Role role) {
+        try (Session session = service.HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.remove(role);
+            transaction.commit();
+            session.close();
+        }
+    }
 
 }
